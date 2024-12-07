86f172d8d19f418b582719378245fdff5dcbeada
chiri <andreichiritescu123@gmail.com>
2024-12-08 01:38:36 +0200

    SideBar and NavBar

---
+++ front/src/components/FrontPage/SideBar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const SideBar = () => {
+  return (
+    <div
+      className="bg-dark text-white"
+      style={{
+        width: "200px",
+        minHeight: "100vh",
+        position: "fixed",
+        top: "60px", // Adjusted for Navbar height
+        left: "0",
+        zIndex: 999, // Keeps the Sidebar on top of other elements
+        overflowY: "auto", // Allows scrolling for long content
+      }}
+    >
+      <ul className="nav flex-column p-3">
+        <li className="nav-item">
+          <a className="nav-link text-white" href="#">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-white" href="#">
+            Categories
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-white" href="#">
+            Favorites
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-white" href="/map">
+            Map
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default SideBar;
